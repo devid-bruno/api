724888322a3c26e5285702f8299b142521f768cc
Devid Bruno <devidbruno@MacBook-Air-de-Devid.local>
2022-09-10 13:36:02 -0300

    db

--- api.js
+++ api.js
@@ -6,7 +6,11 @@ const api = express();
 const port = process.env.PORT || 3000;
 
 api.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+    bank.authenticate().then(() => {
+        res.send('Conectado ao banco');
+    }).catch((msgErr) => {
+        res.send("Unable to connect to the database:", msgErr);
+    });
 });
 
 api.get("/secret", (req, res) => {
