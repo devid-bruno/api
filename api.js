090a280b8d7bc07f6202f322a08032113041d153
Devid Bruno <devidbruno@MacBook-Air-de-Devid.local>
2022-09-10 13:28:55 -0300

    secre?

--- api.js
+++ api.js
@@ -8,6 +8,10 @@ api.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
+api.get("/secret", (req, res) => {
+    res.json({ message: "You found the secret!" });
+});
+
 api.listen(port, () => {
     console.log('Server listening on port 3000');
 });
